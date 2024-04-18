79cdde978422306884ecc5580df14ed58a02267d
nmarks99 <nmarks@anl.gov>
2024-04-18 16:21:47 -0500

    more PVs, trying to make UR dashboard tui

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from textual.app import App
-from textual.widgets import Footer, Header, Label, Static, Button
+from textual.widgets import Footer, Header, Label, Static, Input
 from textual.containers import ScrollableContainer, Horizontal, Vertical, Container, VerticalScroll
 from pvtui import PVLed, PVButton, PVTextMonitor
 
@@ -19,8 +19,9 @@ class ExampleApp(App):
                 with Horizontal(classes="hrow"):
                     yield PVButton("urExample:Dashboard:Connect", label="Connect")
                     yield PVButton("urExample:Dashboard:Disconnect", label="Disconnect")
-                with Horizontal(classes="hrow"):
-                    yield Label("Some more text here ")
+                for _ in range(10):
+                    with Horizontal(classes="hrow"):
+                        yield Label("Some more text here ")
 
             # top right
             with Vertical(id="top-right"):
@@ -33,11 +34,20 @@ class ExampleApp(App):
                 with Horizontal(classes="hrow"):
                     yield Label("Remote Control: ")
                     yield PVLed("urExample:Dashboard:IsInRemoteControl")
+                with Horizontal(classes="hrow"):
+                    yield PVTextMonitor("urExample:Dashboard:RobotMode")
+                with Horizontal(classes="hrow"):
+                    yield PVTextMonitor("urExample:Dashboard:SafetyStatus")
 
     
             # Bottom right
             with Vertical(id="bottom-right"):
-                yield Static("Bottom right")
+                with Horizontal(classes="hrow"):
+                    yield PVTextMonitor("urExample:Dashboard:LoadedProgram")
+                with Horizontal(classes="hrow"):
+                    yield PVTextMonitor("urExample:Dashboard:ProgramState")
+                with Horizontal(classes="hrow"):
+                    yield Input(placeholder="Enter URP program to load...")
 
 if __name__ == "__main__":
     ExampleApp().run()
