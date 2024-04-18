cc8de2f441179821da174b9a1a07a558f0d5302b
nmarks99 <nmarks@anl.gov>
2024-04-18 12:06:53 -0500

    remove old stuff

--- asdf.py
+++
@@ -1,9 +0,0 @@
-from epics import PV
-import time
-import random
-
-pv = PV("min1:value")
-
-for i in range(50):
-    pv.put(round(random.random(), 2))
-    time.sleep(0.2)
--- dev/ftxui-starter/run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd build
-make -j
-cd ../
-./build/ftxui-starter
