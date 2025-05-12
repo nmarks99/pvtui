79aabf68c1c6804c76a00b0c3d426edf153f9728
Nick Marks <nmarks99@gmail.com>
2025-05-11 20:47:40 -0500

    exectuables source code in apps dir, binaries built to build/bin

---
+++ apps/calcout.cpp
@@ -0,0 +1,368 @@
+#include <cassert>
+#include <cstdlib>
+#include <string>
+
+#include <pv/caProvider.h>
+#include <pva/client.h>
+
+#include "ftxui/component/component.hpp"
+#include "ftxui/component/component_base.hpp"
+#include "ftxui/component/loop.hpp"
+#include "ftxui/component/screen_interactive.hpp"
+#include "ftxui/dom/elements.hpp"
+#include "ftxui/screen/color.hpp"
+#include <ftxui/component/event.hpp>
+#include <ftxui/dom/node.hpp>
+
+#include "pvtui.hpp"
+#include "pvgroup.hpp"
+
+using namespace ftxui;
+using namespace pvtui;
+
+struct CalcoutFields {
+    std::string desc;
+    std::string scan;
+    std::string inpa;
+    std::string inpb;
+    std::string inpc;
+    std::string inpd;
+    std::string a;
+    std::string b;
+    std::string c;
+    std::string d;
+    std::string calc;
+    std::string ocal;
+    std::string val;
+    std::string oval;
+    std::string out;
+    std::string flnk;
+    std::string odly;
+    std::string dopt;
+    std::string oopt;
+    std::string ivoa;
+    std::string ivov;
+};
+
+static constexpr std::string_view CLI_HELP_MSG = R"(
+PVTUI calcout - Terminal UI for EPICS calcout record
+
+Usage:
+  pvtui_calcout [options] 
+
+Options:
+  -h, --help                   Show this help message and exit.
+
+Examples:
+    ???
+
+For more details, visit: https://github.com/nmarks99/pvtui
+)";
+
+
+int main(int argc, char *argv[]) {
+  
+    // Parse command line arguments and macros
+    pvtui::ArgParser args(argc, argv);
+
+    if (args.flag("help") or args.flag("h")) {
+	std::cout << CLI_HELP_MSG << std::endl;
+	return EXIT_SUCCESS;
+    }
+
+    if (not args.macros_present({"P", "C"})) {
+	printf("Missing required macros\nRequired macros: P, C\n");
+	return EXIT_FAILURE;
+    }
+
+    // struct of calcout PV names for convenience
+    const CalcoutFields calcout {
+	.desc = args.macros.at("P") + args.macros.at("C") + ".DESC",
+	.scan = args.macros.at("P") + args.macros.at("C") + ".SCAN",
+	.inpa = args.macros.at("P") + args.macros.at("C") + ".INPA",
+	.inpb = args.macros.at("P") + args.macros.at("C") + ".INPB",
+	.inpc = args.macros.at("P") + args.macros.at("C") + ".INPC",
+	.inpd = args.macros.at("P") + args.macros.at("C") + ".INPD",
+	.a = args.macros.at("P") + args.macros.at("C") + ".A",
+	.b = args.macros.at("P") + args.macros.at("C") + ".B",
+	.c = args.macros.at("P") + args.macros.at("C") + ".C",
+	.d = args.macros.at("P") + args.macros.at("C") + ".D",
+	.calc = args.macros.at("P") + args.macros.at("C") + ".CALC",
+	.ocal = args.macros.at("P") + args.macros.at("C") + ".OCAL",
+	.val = args.macros.at("P") + args.macros.at("C") + ".VAL",
+	.oval = args.macros.at("P") + args.macros.at("C") + ".OVAL",
+	.out = args.macros.at("P") + args.macros.at("C") + ".OUT",
+	.flnk = args.macros.at("P") + args.macros.at("C") + ".FLNK",
+	.odly = args.macros.at("P") + args.macros.at("C") + ".ODLY",
+	.dopt = args.macros.at("P") + args.macros.at("C") + ".DOPT",
+	.oopt = args.macros.at("P") + args.macros.at("C") + ".OOPT",
+	.ivoa = args.macros.at("P") + args.macros.at("C") + ".IVOA",
+	.ivov = args.macros.at("P") + args.macros.at("C") + ".IVOV",
+    };
+
+    // Create the FTXUI screen. Interactive and uses the full terminal screen
+    auto screen = ScreenInteractive::Fullscreen();
+
+    // Instantiate EPICS client
+    epics::pvAccess::ca::CAClientFactory::start();
+    pvac::ClientProvider provider(args.provider);
+
+    // PVGroup to connect to all the PVs we need and manager monitors
+    PVGroup pvgroup(provider, {
+	calcout.desc,
+	calcout.scan,
+	calcout.inpa,
+	calcout.inpb,
+	calcout.inpc,
+	calcout.inpd,
+	calcout.a,
+	calcout.b,
+	calcout.c,
+	calcout.d,
+	calcout.calc,
+	calcout.ocal,
+	calcout.val,
+	calcout.oval,
+	calcout.out,
+	calcout.flnk,
+	calcout.odly,
+	calcout.dopt,
+	calcout.oopt,
+	calcout.ivoa,
+	calcout.ivov,
+    });
+
+    // Create monitors and ftxui components for all the PVs we will use.
+    // Note any PVs used below must be defined in the pvgroup in order
+    // for monitor to work.
+    std::string scan;
+    std::vector<std::string> scan_menu_labels{"Passive", "Event", "I/O Intr",
+	"10 second", "5 second", "2 second",
+	"1 second", ".5 second", ".2 second", ".1 second"};
+    PVEnum scan_enum;
+    auto scan_menu = PVDropdown(pvgroup.get_pv(calcout.scan), scan_menu_labels, scan_enum.index);
+    pvgroup.set_monitor(calcout.scan, scan_enum);
+    
+    std::string desc = "";
+    auto desc_input = PVInput(pvgroup[calcout.desc], desc, PVPutType::String);
+    pvgroup.set_monitor(calcout.desc, desc);
+
+    std::string inpa = "";
+    auto inpa_input = PVInput(pvgroup[calcout.inpa], inpa, PVPutType::String);
+    pvgroup.set_monitor(calcout.inpa, inpa);
+
+    std::string a_val;
+    auto a_input = PVInput(pvgroup[calcout.a], a_val, PVPutType::Double);
+    pvgroup.set_monitor(calcout.a, a_val);
+
+    std::string inpb = "";
+    auto inpb_input = PVInput(pvgroup[calcout.inpb], inpb, PVPutType::String);
+    pvgroup.set_monitor<std::string>(calcout.inpb, inpb);
+    
+    std::string b_val;
+    auto b_input = PVInput(pvgroup[calcout.b], b_val, PVPutType::Double);
+    pvgroup.set_monitor(calcout.b, b_val);
+    
+    std::string inpc = "";
+    auto inpc_input = PVInput(pvgroup[calcout.inpc], inpc, PVPutType::String);
+    pvgroup.set_monitor<std::string>(calcout.inpc, inpc);
+    
+    std::string c_val;
+    auto c_input = PVInput(pvgroup[calcout.c], c_val, PVPutType::Double);
+    pvgroup.set_monitor(calcout.c, c_val);
+   
+    std::string inpd = "";
+    auto inpd_input = PVInput(pvgroup[calcout.inpd], inpd, PVPutType::String);
+    pvgroup.set_monitor<std::string>(calcout.inpd, inpd);
+    
+    std::string d_val;
+    auto d_input = PVInput(pvgroup[calcout.d], d_val, PVPutType::Double);
+    pvgroup.set_monitor(calcout.d, d_val);
+
+    std::string calc;
+    auto calc_input = PVInput(pvgroup[calcout.calc], calc, PVPutType::String);
+    pvgroup.set_monitor(calcout.calc, calc);
+
+    std::string ocal;
+    auto ocal_input = PVInput(pvgroup[calcout.ocal], ocal, PVPutType::String);
+    pvgroup.set_monitor(calcout.ocal, ocal);
+
+    std::string out;
+    auto out_input = PVInput(pvgroup[calcout.out], out, PVPutType::String);
+    pvgroup.set_monitor(calcout.out, out);
+
+    std::string flnk;
+    auto flnk_input = PVInput(pvgroup[calcout.flnk], flnk, PVPutType::String);
+    pvgroup.set_monitor(calcout.flnk, flnk);
+
+    std::string val_rbv;
+    pvgroup.set_monitor(calcout.val, val_rbv);
+    
+    std::string oval_rbv;
+    pvgroup.set_monitor(calcout.oval, oval_rbv);
+
+    std::vector<std::string> dopt_menu_labels{"Use CALC", "Use OCAL"};
+    PVEnum dopt_enum;
+    auto dopt_menu = PVDropdown(pvgroup.get_pv(calcout.dopt), dopt_menu_labels, dopt_enum.index);
+    pvgroup.set_monitor(calcout.dopt, dopt_enum);
+
+    std::vector<std::string> ivoa_menu_labels{"Continue normally", "Don't drive outputs", "Set output to IVOV"};
+    PVEnum ivoa_enum;
+    auto ivoa_menu = PVDropdown(pvgroup.get_pv(calcout.ivoa), ivoa_menu_labels, ivoa_enum.index);
+    pvgroup.set_monitor(calcout.ivoa, ivoa_enum);
+    
+    std::vector<std::string> oopt_menu_labels{"Every Time", "On Change",
+	"When Zero", "When Non-zero",
+	"Transition To Zero", "Transition To Non-zero"
+    };
+    PVEnum oopt_enum;
+    auto oopt_menu = PVDropdown(pvgroup.get_pv(calcout.oopt), oopt_menu_labels, oopt_enum.index);
+    pvgroup.set_monitor(calcout.oopt, oopt_enum);
+
+    std::string odly;
+    auto odly_input = PVInput(pvgroup.get_pv(calcout.odly), odly, PVPutType::Double);
+    pvgroup.set_monitor(calcout.odly, odly);
+
+    std::string ivov;
+    auto ivov_input = PVInput(pvgroup.get_pv(calcout.ivov), ivov, PVPutType::Double);
+    pvgroup.set_monitor(calcout.ivov, ivov);
+
+    // Main container to define interactivity of components
+    auto main_container = Container::Vertical({
+	desc_input,
+	scan_menu,
+	Container::Horizontal({
+	    inpa_input, a_input
+	}),
+	Container::Horizontal({
+	    inpb_input, b_input
+	}),
+	Container::Horizontal({
+	    inpc_input, c_input
+	}),
+	Container::Horizontal({
+	    inpd_input, d_input
+	}),
+	calc_input,
+	ocal_input,
+	Container::Horizontal({
+	    odly_input, oopt_menu, dopt_menu
+	}),
+	Container::Horizontal({
+	    ivoa_menu, ivov_input, out_input, flnk_input,
+	})
+    });
+
+    // // Event handler for main container
+    // main_container |= CatchEvent([&](Event event) {
+        // if (event == Event::Character('q')) {
+            // screen.Exit();
+            // return true;
+        // }
+        // return false;
+    // });
+    
+
+    // Main renderer to define visual layout of components and elements
+    auto main_renderer = Renderer(main_container, [&] {
+        return vbox({
+	    hbox({
+		desc_input->Render() | color(Color::Black) |  bgcolor(Color::RGB(210,210,210)) | size(WIDTH, LESS_THAN, 32) | xflex,
+		separatorEmpty(),
+		text("(" + args.macros.at("P")+args.macros.at("C") + ")") | color(Color::Black)
+	    }),
+	    separatorEmpty(),
+	    scan_menu->Render() | EPICSColor::EDIT | size(WIDTH, EQUAL, 10) | color(Color::Black),
+	    separatorEmpty(),
+
+	    hbox({
+		text("A") | color(Color::Black),
+		separatorEmpty(),
+		inpa_input->Render() | size(WIDTH, EQUAL, 32) | EPICSColor::LINK,
+		separatorEmpty(),
+		a_input->Render() | size(WIDTH, EQUAL, 13) | EPICSColor::EDIT
+	    }),
+	    separatorEmpty(),
+	    hbox({
+		text("B") | color(Color::Black),
+		separatorEmpty(),
+		inpb_input->Render() | size(WIDTH, EQUAL, 32) | EPICSColor::LINK,
+		separatorEmpty(),
+		b_input->Render() | size(WIDTH, EQUAL, 13) | EPICSColor::EDIT
+	    }),
+	    separatorEmpty(),
+	    hbox({
+		text("C") | color(Color::Black),
+		separatorEmpty(),
+		inpc_input->Render() | size(WIDTH, EQUAL, 32) | EPICSColor::LINK,
+		separatorEmpty(),
+		c_input->Render() | size(WIDTH, EQUAL, 13) | EPICSColor::EDIT
+	    }),
+	    separatorEmpty(),
+	    hbox({
+		text("D") | color(Color::Black),
+		separatorEmpty(),
+		inpd_input->Render() | size(WIDTH, EQUAL, 32) | EPICSColor::LINK,
+		separatorEmpty(),
+		d_input->Render() | size(WIDTH, EQUAL, 13) | EPICSColor::EDIT
+	    }),
+
+	    separator() | color(Color::Black),
+	    
+	    hbox({
+		text("CALC") | color(Color::Black),
+		filler() | size(WIDTH, EQUAL, 2),
+		calc_input->Render() | size(WIDTH, EQUAL, 32) | EPICSColor::EDIT,
+		separatorEmpty(),
+		text("   " + val_rbv) | EPICSColor::READBACK,
+	    }) | (dopt_enum.index == 0 ? border : borderEmpty) | color(Color::Black),
+
+	    hbox({
+		text("OCAL") | color(Color::Black),
+		filler() | size(WIDTH, EQUAL, 2),
+		ocal_input->Render() | size(WIDTH, EQUAL, 32) | EPICSColor::EDIT,
+		separatorEmpty(),
+		text("   " + oval_rbv) | EPICSColor::READBACK,
+	    }) | (dopt_enum.index == 1 ? border : borderEmpty) | color(Color::Black),
+
+	    separator() | color(Color::Black),
+
+	    hbox({
+		text("ODLY ") | color(Color::Black),
+		odly_input->Render() | size(WIDTH, EQUAL, 4) | color(Color::Black) | bgcolor(Color::RGB(245, 179, 27)),
+		filler() | size(WIDTH, EQUAL, 4),
+		oopt_menu->Render() | size(WIDTH, EQUAL, 25) | color(Color::Black) | bgcolor(Color::RGB(245, 179, 27)),
+		separatorEmpty(),
+		dopt_menu->Render() | size(WIDTH, EQUAL, 10) | color(Color::Yellow) | bgcolor(Color::RGB(130, 95, 16)) | xflex,
+	    }),
+
+	    separatorEmpty(),
+
+	    hbox({
+		ivoa_menu->Render() | color(Color::Yellow) | bgcolor(Color::RGB(130, 95, 16)) | size(WIDTH, EQUAL, 15),
+		text("  IVOV ") | color(Color::Black),
+		ivov_input->Render() | color(Color::Black) | bgcolor(Color::RGB(245, 179, 27)) | size(WIDTH, EQUAL, 4),
+		text("  OUT ") | color(Color::Black),
+		out_input->Render() | EPICSColor::LINK | xflex
+	    }),
+	    separatorEmpty(),
+	    hbox({
+		text("FLNK ") | color(Color::Black),
+		flnk_input->Render() | size(WIDTH, EQUAL, 18) | EPICSColor::LINK
+	    }),
+
+	}) | center | bgcolor(Color::RGB(196,196,196));
+    });
+
+    // Custom main loop
+    constexpr int POLL_PERIOD_MS = 100;
+    Loop loop(&screen, main_renderer);
+    while (!loop.HasQuitted()) {
+	pvgroup.update();
+        screen.PostEvent(Event::Custom);
+        loop.RunOnce();
+        std::this_thread::sleep_for(std::chrono::milliseconds(POLL_PERIOD_MS));
+    }
+
+    return EXIT_SUCCESS;
+}
---
+++ apps/motor.cpp
@@ -0,0 +1,370 @@
+#include <cassert>
+#include <cstdlib>
+#include <string>
+
+#include <pv/caProvider.h>
+#include <pva/client.h>
+
+#include "ftxui/component/component.hpp"
+#include "ftxui/component/component_options.hpp"
+#include "ftxui/component/component_base.hpp"
+#include "ftxui/component/loop.hpp"
+#include "ftxui/component/screen_interactive.hpp"
+#include "ftxui/dom/elements.hpp"
+#include "ftxui/screen/color.hpp"
+#include <ftxui/component/event.hpp>
+#include <ftxui/dom/node.hpp>
+
+#include "pvtui.hpp"
+#include "pvgroup.hpp"
+
+using namespace ftxui;
+using namespace pvtui;
+
+struct MotorFields {
+    std::string desc;
+    std::string rbv;
+    std::string drbv;
+    std::string val;
+    std::string dval;
+    std::string twr;
+    std::string twv;
+    std::string twf;
+    std::string hlm;
+    std::string llm;
+    std::string dhlm;
+    std::string dllm;
+    std::string egu;
+    std::string set;
+    std::string hls;
+    std::string lls;
+    std::string spmg;
+    std::string stop;
+    std::string dmov;
+    std::string able;
+};
+
+static constexpr std::string_view CLI_HELP_MSG = R"(
+PVTUI Motor - Terminal UI for EPICS motors
+
+Usage:
+  pvtui_motor [options] 
+
+Options:
+  -h, --help                   Show this help message and exit.
+  -m, --macro "k1=v1,k2=v2..." Define a macro variable. Same format as MEDM or caQtDM
+  --provider                   EPICS provider, either Channel Access("ca") or PVAccess("pva")
+  --more                       Similar to motorx_more.adl
+  --all                        Similar to motorx_all.adl
+
+Examples:
+  pvtui_motor --macro "P=xxx:,M=m1"
+    Start TUI display for motor xxx:m1 with a style similar to motorx.adl
+  
+  pvtui_motor --more --macro "P=xxx:,M=m1"
+    Start TUI display for motor xxx:m1 with a style similar to motorx_more.adl
+
+For more details, visit: https://github.com/nmarks99/pvtui
+)";
+
+
+int main(int argc, char *argv[]) {
+  
+    // Parse command line arguments and macros
+    pvtui::ArgParser args(argc, argv);
+
+    if (args.flag("help") or args.flag("h")) {
+	std::cout << CLI_HELP_MSG << std::endl;
+	return EXIT_SUCCESS;
+    }
+
+    if (not args.macros_present({"P", "M"})) {
+	printf("Missing required macros\nRequired macros: P, M\n");
+	return EXIT_FAILURE;
+    }
+
+    // struct of motor PV names for convenience
+    const MotorFields motor {
+	.desc = args.macros.at("P") + args.macros.at("M") + ".DESC",
+	.rbv  = args.macros.at("P") + args.macros.at("M") + ".RBV",
+	.drbv = args.macros.at("P") + args.macros.at("M") + ".DRBV",
+	.val  = args.macros.at("P") + args.macros.at("M") + ".VAL",
+	.dval = args.macros.at("P") + args.macros.at("M") + ".DVAL",
+	.twr  = args.macros.at("P") + args.macros.at("M") + ".TWR",
+	.twv  = args.macros.at("P") + args.macros.at("M") + ".TWV",
+	.twf  = args.macros.at("P") + args.macros.at("M") + ".TWF",
+	.hlm  = args.macros.at("P") + args.macros.at("M") + ".HLM",
+	.llm  = args.macros.at("P") + args.macros.at("M") + ".LLM",
+	.dhlm = args.macros.at("P") + args.macros.at("M") + ".DHLM",
+	.dllm = args.macros.at("P") + args.macros.at("M") + ".DLLM",
+	.egu  = args.macros.at("P") + args.macros.at("M") + ".EGU",
+	.set  = args.macros.at("P") + args.macros.at("M") + ".SET",
+	.hls  = args.macros.at("P") + args.macros.at("M") + ".HLS",
+	.lls  = args.macros.at("P") + args.macros.at("M") + ".LLS",
+	.spmg = args.macros.at("P") + args.macros.at("M") + ".SPMG",
+	.stop = args.macros.at("P") + args.macros.at("M") + ".STOP",
+	.dmov = args.macros.at("P") + args.macros.at("M") + ".DMOV",
+	.able = args.macros.at("P") + args.macros.at("M") + "_able",
+    };
+
+    // Create the FTXUI screen. Interactive and uses the full terminal screen
+    auto screen = ScreenInteractive::Fullscreen();
+
+    // Instantiate EPICS client
+    epics::pvAccess::ca::CAClientFactory::start();
+    pvac::ClientProvider provider(args.provider);
+
+    // PVGroup to connect to all the PVs we need and manager monitors
+    PVGroup pvgroup(provider, {
+	motor.val,
+	motor.rbv,
+	motor.dval,
+	motor.drbv,
+	motor.twv,
+	motor.twf,
+	motor.twr,
+	motor.desc,
+	motor.hlm,
+	motor.llm,
+	motor.dhlm,
+	motor.dllm,
+	motor.egu,
+	motor.set,
+	motor.hls,
+	motor.lls,
+	motor.spmg,
+	motor.stop,
+	motor.dmov,
+	motor.able
+    });
+
+    // Create monitors and ftxui components for all the PVs we will use.
+    // Note any PVs used below must be defined in the pvgroup in order
+    // for monitor to work.
+
+    // tweak buttons, don't need readback
+    auto twf_button = PVButton(pvgroup[motor.twf], " > ", 1);
+    auto twr_button = PVButton(pvgroup[motor.twr], " < ", 1);
+
+    // adjustment to the default way PVInput renders
+    auto tform = [](ftxui::InputState s) {
+	return s.element | center | color(Color::Black);
+    };
+
+    // user value input and monitor
+    std::string val_string;
+    auto val_input = PVInput(pvgroup[motor.val], val_string, PVPutType::Double, tform);
+    pvgroup.set_monitor(motor.val, val_string);
+
+    // user high limit input and monitor
+    std::string hlm_string;
+    auto hlm_input = PVInput(pvgroup[motor.hlm], hlm_string, PVPutType::Double, tform);
+    pvgroup.set_monitor(motor.hlm, hlm_string);
+
+    // user low limit input and monitor
+    std::string llm_string;
+    auto llm_input = PVInput(pvgroup[motor.llm], llm_string, PVPutType::Double, tform);
+    pvgroup.set_monitor(motor.llm, llm_string);
+
+    // dial high limit input and monitor
+    std::string dhlm_string;
+    auto dhlm_input = PVInput(pvgroup[motor.dhlm], dhlm_string, PVPutType::Double, tform);
+    pvgroup.set_monitor(motor.dhlm, dhlm_string);
+
+    // dial low limit input and monitor
+    std::string dllm_string;
+    auto dllm_input = PVInput(pvgroup[motor.dllm], dllm_string, PVPutType::Double, tform);
+    pvgroup.set_monitor(motor.dllm, dllm_string);
+
+    // dial value input and monitor
+    std::string dval_string;
+    auto dval_input = PVInput(pvgroup[motor.dval], dval_string, PVPutType::Double, tform);
+    pvgroup.set_monitor(motor.dval, dval_string);
+
+    // tweak value and monitor
+    std::string twv_string;
+    auto twv_input = PVInput(pvgroup[motor.twv], twv_string, PVPutType::Double, tform);
+    pvgroup.set_monitor(motor.twv, twv_string);
+
+    // user readback value
+    double rbv = 0.0;
+    pvgroup.set_monitor<double>(motor.rbv, rbv);
+    
+    // dial readback value
+    double drbv = 0.0;
+    pvgroup.set_monitor<double>(motor.drbv, drbv);
+    
+    // done moving flag
+    int dmov = 0;
+    pvgroup.set_monitor<int>(motor.dmov, dmov);
+
+    // EGU string readback
+    std::string egu = "";
+    pvgroup.set_monitor(motor.egu, egu);
+
+    // string description
+    std::string desc = "";
+    auto desc_input = PVInput(pvgroup[motor.desc], desc, PVPutType::String, tform);
+    pvgroup.set_monitor<std::string>(motor.desc, desc);
+
+    // High limit switch
+    int hls = 0;
+    std::string hls_box = unicode::rectangle(2, 1);
+    pvgroup.set_monitor<int>(motor.hls, hls);
+    
+    // Low limit switch
+    int lls = 0;
+    std::string lls_box = unicode::rectangle(2, 1);
+    pvgroup.set_monitor<int>(motor.lls, lls);
+
+    // Enable/disable toggle
+    std::vector<std::string> en_dis_labels {"Enable", "Disable"};
+    PVEnum en_dis_enum;
+    auto en_dis_menu = PVChoiceH(pvgroup[motor.able], en_dis_labels, en_dis_enum.index);
+    pvgroup.set_monitor(motor.able, en_dis_enum);
+
+    // Use/Set toggle
+    std::vector<std::string> use_set_labels {"Use", "Set"};
+    PVEnum use_set_enum;
+    auto use_set_menu = PVChoiceH(pvgroup[motor.set], use_set_labels, use_set_enum.index);
+    pvgroup.set_monitor(motor.set, use_set_enum);
+
+    // Stop, pause, move, go toggle
+    std::vector<std::string> spmg_labels {"Stop", "Pause ", "Move", "Go"};
+    PVEnum spmg_enum;
+    auto spmg_menu = PVChoiceV(pvgroup[motor.spmg], spmg_labels, spmg_enum.index);
+    pvgroup.set_monitor(motor.spmg, spmg_enum);
+
+    // use this PV for connection status. We assume if we can connect
+    // to this, we can connect to all the PVs for this display
+    auto &desc_pv = pvgroup[motor.desc];
+
+    // Main container to define interactivity of components
+    auto main_container = Container::Vertical({
+	desc_input,
+	Container::Horizontal({
+	    Container::Vertical({
+		hlm_input,
+		val_input,
+		llm_input,
+	    }),
+	    Container::Vertical({
+		dhlm_input,
+		dval_input,
+		dllm_input,
+	    }),
+	    Container::Vertical({
+		spmg_menu,
+	    }),
+	}),
+	Container::Horizontal({
+	    twr_button, twv_input, twf_button
+	}),
+	Container::Horizontal({
+	    en_dis_menu, use_set_menu,
+	})
+    });
+
+    // // Event handler for main container
+    // main_container |= CatchEvent([&](Event event) {
+        // if (event == Event::Character('q')) {
+            // screen.Exit();
+            // return true;
+        // }
+        // return false;
+    // });
+
+    Decorator ColorDisabled = bgcolor(Color::RGBA(80,10,4,230)) | color(Color::Black);
+
+    // Main renderer to define visual layout of components and elements
+    auto main_renderer = Renderer(main_container, [&] {
+        return vbox({
+
+	    [&](){
+		if (desc_pv.connected()) {
+		    return text("");
+		} else {
+		    return text("Disconnected") | color(Color::Red);
+		}
+	    }() | center,
+
+	    desc_input->Render() | color(Color::Black) | bgcolor(Color::RGB(210,210,210)) | size(WIDTH, EQUAL, 21) | center,
+	    separatorEmpty(),
+
+	    // 6 column hbox of vbox's
+	    // none | none | user | dial | lims/egu | spmg
+	    hbox({
+		filler() | size(WIDTH, EQUAL, egu.length()+8),
+		vbox({
+		    text("User") | center,
+		    hlm_input->Render()  | EPICSColor::EDIT | size(WIDTH, EQUAL, 10),
+		    separatorEmpty(), 	
+		    text(std::to_string(rbv)) | (use_set_enum.index==0 ? EPICSColor::READBACK : color(Color::Yellow2)) | center,
+		    separatorEmpty(), 	
+		    val_input->Render()  | (en_dis_enum.index==0 ? EPICSColor::EDIT : ColorDisabled) | size(WIDTH, EQUAL, 10) | size(HEIGHT, EQUAL, 2),
+		    separatorEmpty(), 	
+		    llm_input->Render()  | EPICSColor::EDIT | size(WIDTH, EQUAL, 10),
+		}),
+		separatorEmpty(), 	
+		vbox({
+		    text("Dial") | center,
+		    dhlm_input->Render()  | EPICSColor::EDIT | size(WIDTH, EQUAL, 10),
+		    separatorEmpty(), 	
+		    text(std::to_string(drbv)) | (use_set_enum.index==0 ? EPICSColor::READBACK : color(Color::Yellow2)) | center,
+		    separatorEmpty(), 	
+		    dval_input->Render()  | (en_dis_enum.index==0 ? EPICSColor::EDIT : ColorDisabled) | size(WIDTH, EQUAL, 10) | size(HEIGHT, EQUAL, 2),
+		    separatorEmpty(), 	
+		    dllm_input->Render()  | EPICSColor::EDIT | size(WIDTH, EQUAL, 10),
+		}),
+		separatorEmpty(),
+		vbox({
+		    separatorEmpty(),
+		    text(hls ? unicode::rectangle(2,1) : "") | color(Color::Red),
+		    filler(),
+		    text(egu) | EPICSColor::READBACK,
+		    separatorEmpty(),
+		    text(lls ? unicode::rectangle(2,1) : "") | color(Color::Red),
+		}) | size(WIDTH, EQUAL, egu.length()),
+		separatorEmpty(),
+		vbox({
+		    text(dmov ? "" : "Moving") | color(Color::DarkGreen) | italic | bold,
+		    spmg_menu->Render() | EPICSColor::EDIT | center,
+		    separatorEmpty(),
+		}),
+		separatorEmpty(),
+	    }) | center,
+	    
+	    separatorEmpty(), 	
+
+	    hbox({
+		twr_button->Render() | color(Color::Black),
+		separatorEmpty(),
+		twv_input->Render() | EPICSColor::EDIT | size(WIDTH, EQUAL, 11) | center,
+		separatorEmpty(),
+		twf_button->Render() | color(Color::Black),
+	    }) | center,
+
+	    separatorEmpty(),
+
+	    hbox({
+		separatorEmpty(),
+		use_set_menu->Render() | EPICSColor::EDIT,
+		separatorEmpty(),
+		en_dis_menu->Render() | EPICSColor::EDIT,
+	    }) | center,
+
+	}) | center | bgcolor(Color::RGB(196,196,196));
+    });
+
+    // Custom main loop
+    constexpr int POLL_PERIOD_MS = 100;
+    Loop loop(&screen, main_renderer);
+    while (!loop.HasQuitted()) {
+	pvgroup.update();
+        screen.PostEvent(Event::Custom);
+        loop.RunOnce();
+        std::this_thread::sleep_for(std::chrono::milliseconds(POLL_PERIOD_MS));
+    }
+
+    return EXIT_SUCCESS;
+}
