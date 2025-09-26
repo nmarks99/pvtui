89a39106cc7354c899aadf45df074993146c08e3
Nick Marks <nmarks99@gmail.com>
2025-09-26 17:34:00 -0500

    update test_pvgroup to use mutexes correct

--- tests/test_pvgroup.cpp
+++ tests/test_pvgroup.cpp
@@ -1,115 +1,71 @@
-#include <pv/caProvider.h>
-#include <pva/client.h>
-
-#include <csignal>
-#include <ostream>
+#include <iostream>
+#include <mutex>
 #include <chrono>
 #include <thread>
+#include <csignal>
 
+#include <pv/caProvider.h>
+#include <pva/client.h>
 #include <pvtui/pvgroup.hpp>
 
+// To catch CTRL+C to quit
 volatile std::sig_atomic_t g_signal_caught = 0;
-// Signal handler function
 void signal_handler(int signal) {
     if (signal == SIGINT) {
-	std::cout << "\nKeyboard interrupt (Ctrl+C)" << std::endl;
-	g_signal_caught = 1;
+        std::cout << "\nKeyboard interrupt (Ctrl+C)" << std::endl;
+        g_signal_caught = 1;
     }
 }
 
-int main() {
 
-    // Register the signal handler for SIGINT
-    if (signal(SIGINT, signal_handler) == SIG_ERR) {
-	perror("signal");
-	return 1;
+template <typename T>
+struct DoubleBuffer {
+    T pv_value;
+    T app_value;
+    std::mutex mutex;
+};
+
+DoubleBuffer<double> rbv;
+DoubleBuffer<std::string> desc;
+
+int main(int argc, char *argv[]) {
+
+    std::string prefix;
+    if (argc < 2) {
+	std::cout << "Please provide IOC prefix" << std::endl;
+	return EXIT_FAILURE;
+    } else {
+	prefix = argv[1];
     }
 
-    // Instantiate EPICS client
+    signal(SIGINT, signal_handler);
+
+    // Start the client
     epics::pvAccess::ca::CAClientFactory::start();
     pvac::ClientProvider provider("ca");
 
-    PVGroup pvgroup(provider, {
-	"nmarksTestIOC:m1.VAL",
-	"nmarksTestIOC:m1.RBV",
-	"nmarksTestIOC:m1.TWV",
-	"nmarksTestIOC:m1.TWF",
-	"nmarksTestIOC:m1.TWR",
-	"nmarksTestIOC:m1.PREC",
-	"nmarksTestIOC:m1.DESC",
-	"nmarksTestIOC:m1.SPMG",
-	"nmarksTestIOC:m1.VELO",
-	"nmarksTestIOC:long",
-	"nmarksTestIOC:string",
-	"nmarksTestIOC:float",
-	"nmarksTestIOC:enum",
-	"nmarksTestIOC:double_array",
-	"nmarksTestIOC:long_array",
-	"nmarksTestIOC:string_array"
+    // Create the group and add our PVs
+    PVGroup pvgroup(provider);
+
+    pvgroup.add(prefix + "m1.DESC");
+    pvgroup.set_monitor(prefix + "m1.DESC", desc.pv_value);
+    pvgroup.add_sync_callback(prefix+"m1.DESC", [&](){
+	std::lock_guard<std::mutex> lock(desc.mutex);
+	desc.app_value = desc.pv_value;
     });
 
-    double rbv = 0;
-    pvgroup.set_monitor("nmarksTestIOC:m1.RBV", rbv);
-
-    int prec = 0;
-    pvgroup.set_monitor("nmarksTestIOC:m1.PREC", prec);
-
-    std::string desc = "";
-    pvgroup.set_monitor("nmarksTestIOC:m1.DESC", desc);
-
-    PVEnum spmg;
-    pvgroup.set_monitor("nmarksTestIOC:m1.SPMG", spmg);
-
-    // almost any PV's value can be dumped to a string
-    std::string velo_any;
-    pvgroup.set_monitor("nmarksTestIOC:m1.VELO", velo_any);
-
-    int long_val;
-    pvgroup.set_monitor("nmarksTestIOC:long", long_val);
-
-    std::vector<double> double_arr;
-    pvgroup.set_monitor("nmarksTestIOC:double_array", double_arr);
-
-    std::vector<int> long_arr;
-    pvgroup.set_monitor("nmarksTestIOC:long_array", long_arr);
-
-    std::vector<std::string> string_arr;
-    pvgroup.set_monitor("nmarksTestIOC:string_array", string_arr);
+    pvgroup.add(prefix + "m1.RBV");
+    pvgroup.set_monitor(prefix + "m1.RBV", rbv.pv_value);
+    pvgroup.add_sync_callback(prefix+"m1.RBV", [&](){
+	std::lock_guard<std::mutex> lock(rbv.mutex);
+	rbv.app_value = rbv.pv_value;
+    });
 
     while(g_signal_caught == 0) {
-	std::cout << "nmarksTestIOC:m1.RBV = " << rbv << std::endl;
-
-	std::cout << "nmarksTestIOC:m1.DESC = " << desc << std::endl;
-
-	std::cout << "nmarksTestIOC:m1.PREC = " << prec << std::endl;
-
-	std::cout << "nmarksTestIOC:m1.SPMG = " << spmg.choice << std::endl;
-
-	std::cout << "nmarksTestIOC:m1.VELO (string) = " << velo_any << std::endl;
-
-	std::cout << "nmarksTestIOC:long = " << long_val << std::endl;
-
-	std::cout << "nmarksTestIOC:double_array = ";
-	for (const auto &v : double_arr) {
-	    std::cout << v << " ";
+	if (pvgroup.data_available()) {
+	    std::cout << "DESC = " << desc.app_value << std::endl;
+	    std::cout << "RBV = " << rbv.app_value << std::endl;
 	}
-	std::cout << "\n";
-
-	std::cout << "nmarksTestIOC:long_array = ";
-	for (const auto &v : long_arr) {
-	    std::cout << v << " ";
-	}
-	std::cout << "\n";
-
-	std::cout << "nmarksTestIOC:string_array = ";
-	for (const auto &v : string_arr) {
-	    std::cout << v << " ";
-	}
-	std::cout << "\n";
-
-	std::cout << std::endl;
-	std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
-
 }
-
