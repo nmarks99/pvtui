8b716a86f59944822108743bdc935ca22f3837bf
Nick Marks <nmarks99@gmail.com>
2025-12-15 16:47:28 -0600

    transform record screen

---
+++ apps/transform.cpp
@@ -0,0 +1,223 @@
+#include <pv/caProvider.h>
+#include <pva/client.h>
+
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/loop.hpp>
+#include <ftxui/component/event.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/color.hpp>
+
+#include <memory>
+#include <pvtui/pvtui.hpp>
+#include <pvtui/display_base.hpp>
+
+using namespace ftxui;
+using namespace pvtui;
+
+static constexpr std::string_view CLI_HELP_MSG = R"(
+pvtui_transform - Terminal UI for EPICS transform record
+Inspired by MEDM transform record screens.
+
+Usage:
+  pvtui_transform [options]
+
+Options:
+  -h, --help        Show this help message and exit.
+  -m, --macro       Macros to pass to the UI (required: P, T)
+
+Examples:
+    pvtui_transform --macro "P=xxx:,C=userTran1"
+
+For more details, visit: https://github.com/nmarks99/pvtui
+)";
+
+
+class TransformRow : public DisplayBase {
+  public:
+    TransformRow(PVGroup &pvgroup, const pvtui::ArgParser &args, const std::string &row_name)
+	: DisplayBase(pvgroup),
+	cmtx(pvgroup, args, std::string("$(P)$(T).CMT")+row_name, pvtui::PVPutType::String),
+	inpx(pvgroup, args, std::string("$(P)$(T).INP")+row_name, pvtui::PVPutType::String),
+	clcx(pvgroup, args, std::string("$(P)$(T).CLC")+row_name, pvtui::PVPutType::String),
+	valx(pvgroup, args, std::string("$(P)$(T).")+row_name, pvtui::PVPutType::Double),
+	outx(pvgroup, args, std::string("$(P)$(T).OUT")+row_name, pvtui::PVPutType::Double)
+    {}
+
+    ~TransformRow() override = default;
+
+    ftxui::Component get_container() override {
+	return Container::Vertical({
+	    cmtx.component(),
+	    inpx.component(),
+	    clcx.component(),
+	    valx.component(),
+	    outx.component()
+	});
+    }
+
+    ftxui::Element get_renderer() override {
+	return hbox({
+	    separator() | color(Color::Black),
+	    cmtx.component()->Render()
+		| size(WIDTH, EQUAL, 15)
+		| EPICSColor::edit(cmtx),
+	    separator() | color(Color::Black),
+	    inpx.component()->Render()
+		| size(WIDTH, EQUAL, 25)
+		| EPICSColor::link(cmtx),
+	    separator() | color(Color::Black),
+	    clcx.component()->Render()
+		| size(WIDTH, EQUAL, 25)
+		| EPICSColor::edit(cmtx),
+	    separator() | color(Color::Black),
+	    valx.component()->Render()
+		| size(WIDTH, EQUAL, 15)
+		| EPICSColor::edit(cmtx),
+	    separator() | color(Color::Black),
+	    outx.component()->Render()
+		| xflex
+		| EPICSColor::link(cmtx),
+	    separator() | color(Color::Black),
+	});
+    }
+
+  private:
+    InputWidget cmtx;
+    InputWidget inpx;
+    InputWidget clcx;
+    InputWidget valx;
+    InputWidget outx;
+};
+
+int main(int argc, char *argv[]) {
+
+    // Parse command line arguments and macros
+    pvtui::ArgParser args(argc, argv);
+
+    if (args.flag("help") or args.flag("h")) {
+	std::cout << CLI_HELP_MSG << std::endl;
+	return EXIT_SUCCESS;
+    }
+
+    if (not args.macros_present({"P", "T"})) {
+	printf("Missing required macros\nRequired macros: P, T\n");
+	return EXIT_FAILURE;
+    }
+
+    // Create the FTXUI screen. Interactive and uses the full terminal screen
+    auto screen = ScreenInteractive::Fullscreen();
+
+    // Instantiate EPICS client
+    epics::pvAccess::ca::CAClientFactory::start();
+    pvac::ClientProvider provider(args.provider);
+
+    // PVGroup to manage all PVs in the display
+    PVGroup pvgroup(provider);
+
+    ChoiceWidget scan(pvgroup, args, "$(P)$(T).SCAN", ChoiceStyle::Dropdown);
+    ButtonWidget proc(pvgroup, args, "$(P)$(T).PROC", " PROC ");
+    InputWidget desc(pvgroup, args, "$(P)$(T).DESC", PVPutType::String);
+    InputWidget prec(pvgroup, args, "$(P)$(T).PREC", PVPutType::Integer);
+    InputWidget flnk(pvgroup, args, "$(P)$(T).FLNK", PVPutType::String);
+    ChoiceWidget copt(pvgroup, args, "$(P)$(T).COPT", ChoiceStyle::Dropdown);
+
+    std::vector<std::unique_ptr<DisplayBase>> rows;
+    for (char c = 'A'; c <= 'P'; c++) {
+	std::string s {c};
+	rows.emplace_back(std::make_unique<TransformRow>(pvgroup, args, s));
+    }
+
+    // Main container to define interactivity of components
+    auto main_container = Container::Vertical({
+	desc.component(),
+	scan.component(),
+	proc.component(),
+	copt.component(),
+	flnk.component()
+    });
+    for (auto &row : rows) {
+	main_container->Add(row->get_container());
+    }
+
+    // Main renderer to define visual layout of components and elements
+    auto main_renderer = Renderer(main_container, [&] {
+	Elements elements {
+	    desc.component()->Render()
+		| EPICSColor::edit(desc)
+		| size(WIDTH, EQUAL, 25),
+	    separatorEmpty(),
+	    hbox({
+		scan.component()->Render()
+		    | EPICSColor::edit(scan)
+		    | size(WIDTH, EQUAL, 10),
+		filler() | size(WIDTH, EQUAL, 20),
+		proc.component()->Render()
+		    | EPICSColor::edit(proc)
+		    | size(WIDTH, EQUAL, 8),
+		filler() | size(WIDTH, EQUAL, 5),
+		text("PREC: ") | color(Color::Black),
+		prec.component()->Render()
+		    | EPICSColor::edit(prec)
+		    | size(WIDTH, EQUAL, 2),
+		filler() | xflex,
+	    }),
+	    separatorEmpty(),
+	    hbox({
+		text("   Comment")
+		    | size(WIDTH, EQUAL, 15)
+		    | color(Color::Black),
+		text("        In link")
+		    | size(WIDTH, EQUAL, 25)
+		    | color(Color::Black),
+		text("              Calc")
+		    | size(WIDTH, EQUAL, 35)
+		    | color(Color::Black),
+		text("  Value")
+		    | size(WIDTH, EQUAL, 15)
+		    | color(Color::Black),
+		text("       Out link")
+		    | size(WIDTH, EQUAL, 25)
+		    | color(Color::Black),
+	    }),
+	    separator() | color(Color::Black)
+	};
+	for (auto &row : rows) {
+	    elements.push_back(row->get_renderer());
+	    elements.push_back(separator() | color(Color::Black));
+	}
+
+	elements.push_back(
+	    hbox({
+		filler() | xflex,
+		text("Calc option: ") | color(Color::Black),
+		copt.component()->Render()
+		    | EPICSColor::edit(copt)
+		    | size(WIDTH, EQUAL, 15),
+		filler() | size(WIDTH, EQUAL, 5),
+		text("FLNK: ") | color(Color::Black),
+		flnk.component()->Render()
+		    | EPICSColor::link(flnk)
+		    | size(WIDTH, EQUAL, 25),
+		separatorEmpty()
+	    })
+	);
+
+        return vbox({
+	    elements,
+	}) | center | bgcolor(Color::RGB(196,196,196));
+    });
+
+    // Custom main loop
+    constexpr int POLL_PERIOD_MS = 100;
+    Loop loop(&screen, main_renderer);
+    while (!loop.HasQuitted()) {
+	if (pvgroup.sync()) {
+	    screen.PostEvent(Event::Custom);
+	}
+        loop.RunOnce();
+        std::this_thread::sleep_for(std::chrono::milliseconds(POLL_PERIOD_MS));
+    }
+
+    return EXIT_SUCCESS;
+}
