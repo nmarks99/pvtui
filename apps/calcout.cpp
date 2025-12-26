97b7d1082994a73b8bfd1e62090da5c1727a3552
Nick Marks <nmarks99@gmail.com>
2025-12-26 14:23:59 -0600

    testing some ideas

--- apps/calcout.cpp
+++ apps/calcout.cpp
@@ -30,51 +30,78 @@ Examples:
 For more details, visit: https://github.com/nmarks99/pvtui
 )";
 
+pvac::ClientProvider init_epics_provider(const std::string &p) {
+    epics::pvAccess::ca::CAClientFactory::start();
+    pvac::ClientProvider provider(p);
+    return provider;
+}
+
+struct Tui {
+    Tui(int argc, char *argv[]) :
+	args(ArgParser(argc, argv)),
+	provider(init_epics_provider(args.provider)),
+	pvgroup(PVGroup(provider)),
+	screen(ScreenInteractive::Fullscreen()) {}
+
+    void default_main_loop(const Component &renderer, int poll_period_ms) {
+	Loop loop(&screen, renderer);
+	while (!loop.HasQuitted()) {
+	    if (pvgroup.sync()) {
+		screen.PostEvent(Event::Custom);
+	    }
+	    loop.RunOnce();
+	    std::this_thread::sleep_for(std::chrono::milliseconds(poll_period_ms));
+	}
+    }
+
+    void run(const Component &renderer, int poll_period_ms = 100) {
+	if (custom_loop) {
+	    custom_loop(renderer, poll_period_ms);
+	}
+	default_main_loop(renderer, poll_period_ms);
+    }
+
+    std::function<void(const Component &renderer, int poll_period_ms)> custom_loop;
+    ArgParser args;
+    pvac::ClientProvider provider;
+    PVGroup pvgroup;
+    ScreenInteractive screen;
+};
+
 int main(int argc, char *argv[]) {
 
-    // Parse command line arguments and macros
-    pvtui::ArgParser args(argc, argv);
+    Tui app(argc, argv);
 
-    if (args.help(CLI_HELP_MSG)) return EXIT_SUCCESS;
+    if (app.args.help(CLI_HELP_MSG)) return EXIT_SUCCESS;
 
-    if (not args.macros_present({"P", "C"})) {
+    if (not app.args.macros_present({"P", "C"})) {
 	printf("Missing required macros\nRequired macros: P, C\n");
 	return EXIT_FAILURE;
     }
 
-    // Create the FTXUI screen. Interactive and uses the full terminal screen
-    auto screen = ScreenInteractive::Fullscreen();
-
-    // Instantiate EPICS client
-    epics::pvAccess::ca::CAClientFactory::start();
-    pvac::ClientProvider provider(args.provider);
-
-    // PVGroup to manage all PVs in the display
-    PVGroup pvgroup(provider);
-
-    ChoiceWidget scan(pvgroup, args, "$(P)$(C).SCAN", ChoiceStyle::Dropdown);
-    InputWidget desc(pvgroup, args, "$(P)$(C).DESC", PVPutType::String);
-    InputWidget prec(pvgroup, args, "$(P)$(C).PREC", PVPutType::Integer);
-    InputWidget inpa(pvgroup, args, "$(P)$(C).INPA", PVPutType::String);
-    InputWidget a_val(pvgroup, args, "$(P)$(C).A", PVPutType::Double);
-    InputWidget inpb(pvgroup, args, "$(P)$(C).INPB", PVPutType::String);
-    InputWidget b_val(pvgroup, args, "$(P)$(C).B", PVPutType::Double);
-    InputWidget inpc(pvgroup, args, "$(P)$(C).INPC", PVPutType::String);
-    InputWidget c_val(pvgroup, args, "$(P)$(C).C", PVPutType::Double);
-    InputWidget inpd(pvgroup, args, "$(P)$(C).INPD", PVPutType::String);
-    InputWidget d_val(pvgroup, args, "$(P)$(C).D", PVPutType::Double);
-    InputWidget calc(pvgroup, args, "$(P)$(C).CALC", PVPutType::String);
-    InputWidget ocal(pvgroup, args, "$(P)$(C).OCAL", PVPutType::String);
-    InputWidget out(pvgroup, args, "$(P)$(C).OUT", PVPutType::String);
-    InputWidget flnk(pvgroup, args, "$(P)$(C).FLNK", PVPutType::String);
-    VarWidget<std::string> val(pvgroup, args, "$(P)$(C).VAL");
-    VarWidget<std::string> oval(pvgroup, args, "$(P)$(C).OVAL");
-    ChoiceWidget dopt(pvgroup, args, "$(P)$(C).DOPT", ChoiceStyle::Dropdown);
-    ChoiceWidget ivoa(pvgroup, args, "$(P)$(C).IVOA", ChoiceStyle::Dropdown);
-    ChoiceWidget oopt(pvgroup, args, "$(P)$(C).OOPT", ChoiceStyle::Dropdown);
-    InputWidget odly(pvgroup, args, "$(P)$(C).ODLY", PVPutType::Double);
-    InputWidget ivov(pvgroup, args, "$(P)$(C).IVOV", PVPutType::Double);
-    ButtonWidget proc(pvgroup, args, "$(P)$(C).PROC", " PROC ");
+    ChoiceWidget scan(app.pvgroup, app.args, "$(P)$(C).SCAN", ChoiceStyle::Dropdown);
+    InputWidget desc(app.pvgroup, app.args, "$(P)$(C).DESC", PVPutType::String);
+    InputWidget prec(app.pvgroup, app.args, "$(P)$(C).PREC", PVPutType::Integer);
+    InputWidget inpa(app.pvgroup, app.args, "$(P)$(C).INPA", PVPutType::String);
+    InputWidget a_val(app.pvgroup, app.args, "$(P)$(C).A", PVPutType::Double);
+    InputWidget inpb(app.pvgroup, app.args, "$(P)$(C).INPB", PVPutType::String);
+    InputWidget b_val(app.pvgroup, app.args, "$(P)$(C).B", PVPutType::Double);
+    InputWidget inpc(app.pvgroup, app.args, "$(P)$(C).INPC", PVPutType::String);
+    InputWidget c_val(app.pvgroup, app.args, "$(P)$(C).C", PVPutType::Double);
+    InputWidget inpd(app.pvgroup, app.args, "$(P)$(C).INPD", PVPutType::String);
+    InputWidget d_val(app.pvgroup, app.args, "$(P)$(C).D", PVPutType::Double);
+    InputWidget calc(app.pvgroup, app.args, "$(P)$(C).CALC", PVPutType::String);
+    InputWidget ocal(app.pvgroup, app.args, "$(P)$(C).OCAL", PVPutType::String);
+    InputWidget out(app.pvgroup, app.args, "$(P)$(C).OUT", PVPutType::String);
+    InputWidget flnk(app.pvgroup, app.args, "$(P)$(C).FLNK", PVPutType::String);
+    VarWidget<std::string> val(app.pvgroup, app.args, "$(P)$(C).VAL");
+    VarWidget<std::string> oval(app.pvgroup, app.args, "$(P)$(C).OVAL");
+    ChoiceWidget dopt(app.pvgroup, app.args, "$(P)$(C).DOPT", ChoiceStyle::Dropdown);
+    ChoiceWidget ivoa(app.pvgroup, app.args, "$(P)$(C).IVOA", ChoiceStyle::Dropdown);
+    ChoiceWidget oopt(app.pvgroup, app.args, "$(P)$(C).OOPT", ChoiceStyle::Dropdown);
+    InputWidget odly(app.pvgroup, app.args, "$(P)$(C).ODLY", PVPutType::Double);
+    InputWidget ivov(app.pvgroup, app.args, "$(P)$(C).IVOV", PVPutType::Double);
+    ButtonWidget proc(app.pvgroup, app.args, "$(P)$(C).PROC", " PROC ");
 
     // Main container to define interactivity of components
     auto main_container = Container::Vertical({
@@ -101,7 +128,7 @@ int main(int argc, char *argv[]) {
 	    hbox({
 		desc.component()->Render() | color(Color::Black) |  bgcolor(Color::RGB(210,210,210)) | size(WIDTH, LESS_THAN, 32) | xflex,
 		separatorEmpty(),
-		text("(" + args.macros.at("P")+args.macros.at("C") + ")") | color(Color::Black)
+		text("(" + app.args.macros.at("P")+app.args.macros.at("C") + ")") | color(Color::Black)
 	    }),
 	    separatorEmpty(),
 	    hbox({
@@ -211,16 +238,8 @@ int main(int argc, char *argv[]) {
 	}) | center | bgcolor(Color::RGB(196,196,196));
     });
 
-    // Custom main loop
-    constexpr int POLL_PERIOD_MS = 100;
-    Loop loop(&screen, main_renderer);
-    while (!loop.HasQuitted()) {
-	if (pvgroup.sync()) {
-	    screen.PostEvent(Event::Custom);
-	}
-        loop.RunOnce();
-        std::this_thread::sleep_for(std::chrono::milliseconds(POLL_PERIOD_MS));
-    }
+    // Run with default main loop
+    app.run(main_renderer);
 
     return EXIT_SUCCESS;
 }
